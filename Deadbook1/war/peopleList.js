ee43e65412c8a62b834b5ed4b151b642ddccc61c
samuelregev <samuel.regev@gmail.com>
2013-11-16 17:59:09 +0200

    3rd

---
+++ Deadbook1/war/peopleList.js
@@ -0,0 +1,5 @@
+﻿arr = [{name:"פלוני אלמוני1", pic_url:"emptyProfilePic.jpg", desc:"1כמה מילים", pID: 1},
+{name:"פלוני אלמוני2", pic_url:"emptyProfilePic.jpg", desc:"2כמה מילים", pID: 2},
+{name:"פלוני אלמוני3", pic_url:"emptyProfilePic.jpg", desc:"3כמה מילים", pID: 3},
+{name:"פלוני אלמוני4", pic_url:"emptyProfilePic.jpg", desc:"4כמה מילים", pID: 4},
+{name:"פלוני אלמוני5", pic_url:"emptyProfilePic.jpg", desc:"5כמה מילים", pID: 5}];
--- Deakbook1/war/bottom_of_page.jsp
+++
@@ -1,9 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-<!-- end content -->
-				</div>
-		</div><!-- end inner -->
-	</div><!-- end outer -->
- 	<div id="footer"><h1><a href="team.html">copyrights&copy; Deadbook team</a></h1></div>
-</div><!-- end container -->
-</body>
-</html>
